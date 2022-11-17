3aac13b8905d12b3dbd3ae0ecabcd132844bbe1c
Masataka ISHII <masataka.ishii@soat.fr>
2022-11-17 14:53:09 +0100

    first commit

---
+++ src/test/java/fr/soat/trainmeet/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.soat.trainmeet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
