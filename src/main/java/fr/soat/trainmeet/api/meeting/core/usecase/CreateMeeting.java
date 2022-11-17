a7166dd4dbcf507daae3dd74e6ee2b5816d87d6a
Masataka ISHII <masataka.ishii@soat.fr>
2022-11-17 18:16:31 +0100

    create usecase to create meeting and params

---
+++ src/main/java/fr/soat/trainmeet/api/meeting/core/usecase/CreateMeeting.java
@@ -0,0 +1,9 @@
+package fr.soat.trainmeet.api.meeting.core.usecase;
+
+import fr.soat.trainmeet.api.meeting.core.domain.MeetingParams;
+
+public class CreateMeeting {
+    public Long execute(MeetingParams params) {
+        return null;
+    }
+}
