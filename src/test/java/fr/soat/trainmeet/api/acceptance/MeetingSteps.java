7a6743ea7cae0e7673cb71e3517ae6b3b69962a8
Masataka ISHII <masataka.ishii@soat.fr>
2022-11-17 18:16:54 +0100

    meeting steps WIP

--- src/test/java/fr/soat/trainmeet/api/acceptance/MeetingSteps.java
+++ src/test/java/fr/soat/trainmeet/api/acceptance/MeetingSteps.java
@@ -1,13 +1,19 @@
 package fr.soat.trainmeet.api.acceptance;
 
+import fr.soat.trainmeet.api.meeting.infrastructure.JpaAccount;
+import fr.soat.trainmeet.api.meeting.infrastructure.JpaAccountRepository;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.format.DateTimeFormatter;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -16,31 +22,63 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @CucumberContextConfiguration
 @ActiveProfiles("AcceptanceTest")
 public class MeetingSteps {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    @Autowired
+    private JpaAccountRepository jpaAccountRepository;
+
+    private String name;
+    private String senderEmail;
+    private String description;
+    private String firstDate;
+    private String lastDate;
+    private String today;
+    private String receiverEmail;
+
+    @Given("a sender with email {string} that have account")
+    public void aSenderWithEmailThatHaveAccount(String email) {
+        jpaAccountRepository.save(JpaAccount.JpaAccountBuilder.builder().email(email).build());
+        senderEmail = email;
+    }
+
     @Given("a meeting with name {string}")
     public void aMeetingWithName(String name) {
+        this.name = name;
+        System.out.println(this.name);
     }
 
     @And("the description is {string}")
     public void theDescriptionIs(String description) {
+        this.description = description;
     }
 
     @And("the meeting first potential date is {string}")
     public void theMeetingFirstPotentialDateIs(String firstDate) {
+        this.firstDate = firstDate;
     }
 
     @And("the last potential date is {string}")
     public void theLastPotentialDateIs(String lastDate) {
+        this.lastDate = lastDate;
     }
 
     @And("today is {string}")
     public void todayIs(String today) {
+        this.today = today;
     }
 
-    @When("the meeting is organized")
-    public void theMeetingIsOrganized() {
+    @And("a receiver with email {string} that have account")
+    public void theReceiverWithEmailThatHaveAccount(String email) {
+        jpaAccountRepository.save(JpaAccount.JpaAccountBuilder.builder().email(email).build());
+        this.receiverEmail = email;
     }
 
-    @Then("the meeting is created with properties")
-    public void theMeetingIsCreatedWithProperties() {
+    @When("the sender try to create meeting")
+    public void theSenderTryToCreateMeeting() {
+        // TODO : continue to implement the steps
+    }
+
+    @Then("the meeting should be created with properties")
+    public void theMeetingShouldBeCreatedWithProperties(DataTable dataTable) {
     }
 }
--- src/test/resources/fr.soat.trainmeet.api/meeting.feature
+++ src/test/resources/fr.soat.trainmeet.api/meeting.feature
@@ -1,13 +1,15 @@
 Feature: Meeting management
 
-  Scenario: Organize meeting
+  Scenario: Create meeting
     Given a meeting with name "Sign the contract"
     And the description is "the purpose of the meeting is to sign your new contract"
     And the meeting first potential date is "22-09-2022"
     And the last potential date is "30-09-2022"
+    And a sender with email "sender@email.com" that have account
+    And a receiver with email "receiver@email.com" that have account
     And today is "20-09-2022"
-    When the meeting is organized
-    Then the meeting is created with properties
+    When the sender try to create meeting
+    Then the meeting should be created with properties
       | name               | Sign the contract                                      |
       | description        | the purpose of the meeting is to sign you new contract |
       | firstPotentialDate | 22-09-2022                                             |
